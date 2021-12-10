aff9f0d79300f728df703bc5870b806c69ad0b88
GonzalezNacho <nacho.ldm63@gmail.com>
2021-12-10 14:56:41 -0300

    prueba de ruta

--- src/main/java/maybePass/Pantalla.java
+++ src/main/java/maybePass/Pantalla.java
@@ -18,10 +18,17 @@ public class Pantalla implements Dibujable {
             this.ancho = ancho;
             this.largo = largo;
             String path = Paths.get(Pantalla.class.getClassLoader().getResource(resource).toURI()).toString();
-            System.out.println("Working Directory = " + System.getProperty("user.dir")+ path);
             this.img = ImageIO.read(new File(path));
-        } catch (Exception e1) {
-            throw new RuntimeException(e1);
+            System.out.println(System.getProperty("os.name"));
+        } catch  (Exception e){
+            try {
+            	this.ancho = ancho;
+                this.largo = largo;
+                String path2 = System.getProperty("user.dir") + "/target/classes/" + resource;
+                this.img = ImageIO.read(new File(path2));
+            }  catch (Exception e1) {
+                throw new RuntimeException(e1);
+            }
         }
     }
 
