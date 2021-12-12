b7b436216768ddcfc3c5b755d5427f519dff0afe
GonzalezNacho <nacho.ldm63@gmail.com>
2021-12-12 06:00:43 -0300

    monedas implementa dibujable

--- src/main/java/maybePass/Monedas.java
+++ src/main/java/maybePass/Monedas.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
-public class Monedas {
+public class Monedas implements Dibujable{
 	
 	private int posicionX;
     private int posicionY;
