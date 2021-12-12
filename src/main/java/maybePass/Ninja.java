d95a6a6453eecd80b2acd65b3f35c40e51e220b9
GonzalezNacho <nacho.ldm63@gmail.com>
2021-12-12 08:09:19 -0300

    animacion de ninja caminando

--- src/main/java/maybePass/Ninja.java
+++ src/main/java/maybePass/Ninja.java
@@ -12,6 +12,10 @@ import javax.imageio.ImageIO;
 public class Ninja extends ElementoConImagen {
 	
 	private BufferedImage img;
+	private BufferedImage imgWalk1;
+	private BufferedImage imgWalk2;
+	private BufferedImage imgWalk3;
+	private int contador;
 
     public Ninja(int posicionX, int posicionY, double velocidadX, double velocidadY, int ancho, int largo,
             Color color) {
@@ -19,8 +23,14 @@ public class Ninja extends ElementoConImagen {
         //String path = Paths.get(Ninja.class.getClassLoader().getResource("imagenes/ninja.PNG").getPath())
         //        .toString();
         String path = Utilidades.obtenerRuta("imagenes/ninja.PNG");
+        String pathWalk1 = Utilidades.obtenerRuta("imagenes/walk1.PNG");
+        String pathWalk2 = Utilidades.obtenerRuta("imagenes/walk2.PNG");
+        String pathWalk3 = Utilidades.obtenerRuta("imagenes/walk3.PNG");
         try {
-            img = ImageIO.read(new File(path));
+        	img = ImageIO.read(new File(path));
+        	imgWalk1 = ImageIO.read(new File(pathWalk1));
+        	imgWalk2 = ImageIO.read(new File(pathWalk2));
+        	imgWalk3 = ImageIO.read(new File(pathWalk3));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -28,7 +38,26 @@ public class Ninja extends ElementoConImagen {
 
     public void dibujarse(Graphics graphics) {
         try {
-            graphics.drawImage(img, getPosicionX(), getPosicionY(), this.getAncho(), this.getLargo(), null);
+            if (getVelocidadX() == 0 && getVelocidadY() == 0 ) {
+            	graphics.drawImage(img, getPosicionX(), getPosicionY(), this.getAncho(), this.getLargo(), null);
+            } else {
+            	contador++;
+                if (contador == 30) {
+               	 contador = 0;
+                }
+                
+                if (0 <= contador && contador < 10) {
+                    graphics.drawImage(imgWalk1, getPosicionX(), getPosicionY(), this.getAncho(), this.getLargo(), null);
+                }
+                if (10 <= contador  && contador < 20) {
+                    graphics.drawImage(imgWalk2, getPosicionX(), getPosicionY(), this.getAncho(), this.getLargo(), null);
+                }
+               
+                if (20 <= contador  && contador < 30) {
+                    graphics.drawImage(imgWalk3, getPosicionX(), getPosicionY(), this.getAncho(), this.getLargo(), null);
+                }
+            }
+        	
         } catch (Exception e1) {
             throw new RuntimeException(e1);
         }
